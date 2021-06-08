83a603480da0750549fa7723f59ac9ceaafdbb12
Jauharuddin <jauharnotes@gmail.com>
2021-06-08 23:37:45 +0700

    Create komentar.js

---
+++ basic/komentar.js
@@ -0,0 +1,8 @@
+// Ini komentar satu baris
+    console.log("Hello Komentar");
+
+    /*
+    ini komentar lebih dari satu baris
+    bebas mau buat apapun
+     */
+    console.log("Hello Komentar Lebih Dari Satu Baris");
