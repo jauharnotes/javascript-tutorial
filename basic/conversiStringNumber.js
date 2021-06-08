493b91297bb5a96c356c2e4bc1903edd552d74d9
Jauharuddin <jauharnotes@gmail.com>
2021-06-08 23:39:04 +0700

    Create conversiStringNumber.js

---
+++ basic/conversiStringNumber.js
@@ -0,0 +1,29 @@
+const value1 = parseInt("1");
+    const value2 = 1;
+    const sum = value1 + value2;
+
+    document.writeln(`<p>${sum}</p>`)
+
+    document.writeln(`<p>${parseInt("1.9")}</p>`)
+    document.writeln(`<p>${parseFloat("1.1")}</p>`)
+    document.writeln(`<p>${Number("1.1")}</p>`)
+
+    const a = 1;
+    const b = 1;
+    const total = a.toString() + b.toString();
+    document.writeln(`<p>${total}</p>`);
+
+    document.writeln(`<p>${parseInt("1salah")}</p>`)
+    document.writeln(`<p>${parseFloat("1.1eko")}</p>`)
+
+    document.writeln(`<p>${Number("1salah")}</p>`)
+    document.writeln(`<p>${Number("1.1eko")}</p>`)
+    document.writeln(`<p>${Number("salah")}</p>`)
+
+    const first = Number("salah"); // NaN
+    const totalNumber = first + 100;
+    document.writeln(`<p>${totalNumber}</p>`);
+
+    document.writeln(`<p>${isNaN(first)}</p>`);
+    document.writeln(`<p>${isNaN(100)}</p>`);
+    document.writeln(`<p>${isNaN(NaN)}</p>`);
