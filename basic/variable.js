bda3bafbcfbf9c260f8eb4a8116829b79a0ddfc9
Jauharuddin <jauharnotes@gmail.com>
2021-06-08 23:01:50 +0700

    Create variable.js

---
+++ basic/variable.js
@@ -0,0 +1,18 @@
+var fullName = "Eko Kurniawan Khannedy";
+    var firstName = "Eko";
+    var middleName = "Kurniawna";
+    var lastName = "Khannedy";
+
+    document.writeln(fullName);
+    document.writeln("</br>");
+    document.writeln(fullName);
+    document.writeln("</br>");
+    document.writeln(fullName);
+
+    fullName = "Eko Khannedy";
+
+    document.writeln("</br>");
+    document.writeln(fullName);
+    document.writeln("</br>");
+    document.writeln(fullName);
+    document.writeln("</br>");
