3da8600b7220ca930b89a041220b038d2a3ed0d9
jauharnotes <jauharnotes@gmail.com>
2021-06-10 19:57:50 +0700

    string

---
+++ standar_library/string.js
@@ -0,0 +1,11 @@
+const names = "Jauhar Uddin";
+
+console.log(names.length);
+console.log(names.toLowerCase());
+console.log(names.toUpperCase());
+console.log(names.split(" "));
+console.log(names);
+
+const input = "   input data   ";
+console.log(input.trim());
+console.log(input);
