6e74c7ea75422783607f8fe34ac5cb132c61daf0
Jauharuddin <jauharnotes@gmail.com>
2021-06-11 19:36:34 +0700

    Update array.js

--- standar_library/array.js
+++ standar_library/array.js
@@ -44,3 +44,14 @@ console.log("===================");
   console.log(stack.pop());
   console.log(stack.pop());
 }
+
+{
+  // Array Search
+  const source = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5];
+
+  console.log(source.find(value => value > 3));
+  console.log(source.findIndex(value => value > 3));
+  console.log(source.includes(7));
+  console.log(source.indexOf(5));
+  console.log(source.lastIndexOf(5));
+}
