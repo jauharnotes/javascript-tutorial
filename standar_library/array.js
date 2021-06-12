a0d2045898ac0ba3f7e218de58af840e8718df55
jauharnotes <jauharnotes@gmail.com>
2021-06-12 11:15:44 +0700

    array

--- standar_library/array.js
+++ standar_library/array.js
@@ -49,9 +49,40 @@ console.log("===================");
   // Array Search
   const source = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5];
 
-  console.log(source.find(value => value > 3));
-  console.log(source.findIndex(value => value > 3));
+  console.log(source.find((value) => value > 3));
+  console.log(source.findIndex((value) => value > 3));
   console.log(source.includes(7));
   console.log(source.indexOf(5));
   console.log(source.lastIndexOf(5));
+}
+
+{
+  // Array Filter
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const ganjil = numbers.filter((value) => value % 2 == 1);
+  const genab = numbers.filter((value) => value % 2 == 0);
+
+  console.log(ganjil);
+  console.log(genab);
+}
+
+{
+  // Array Transform
+  const names = ["Jauhar", "Uddin", "Khannedy"];
+
+  const nameUpper = names.map((value) => value.toUpperCase());
+  console.log(nameUpper);
+
+  const nameReduce = names.reduce((before, value) => before + " " + value);
+  console.log(nameReduce);
+
+  const nameReduceRight = names.reduceRight(
+    (before, value) => before + " " + value
+  );
+  console.log(nameReduceRight);
+
+  const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+  const total = numbers.reduce((before, value) => before + value);
+  console.log(total);
 }
