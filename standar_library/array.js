3066ce9821ceb093e8b3d4277433e4619cd4c616
jauharnotes <jauharnotes@gmail.com>
2021-06-22 19:35:53 +0700

    array prototype

--- standar_library/array.js
+++ standar_library/array.js
@@ -86,3 +86,66 @@ console.log("===================");
   const total = numbers.reduce((before, value) => before + value);
   console.log(total);
 }
+
+{
+  // Array prototype reverse
+  const foo = ["a", "b", "c", "d", "e"];
+
+  const bar = foo.reverse();
+  console.log(bar);
+  console.log(foo);
+
+  const back = bar.reverse();
+  console.log(back);
+}
+
+{
+  // Array prototype concat
+  const foo = ["a", "b", "c", "d", "e"];
+  const bar = [1, 2, 3, 4, 5];
+
+  const fooBar = foo.concat(bar);
+  console.log(fooBar);
+
+  const barFoo = bar.concat(foo);
+  console.log(barFoo);
+
+  console.log(foo);
+  console.log(bar);
+}
+
+{
+  // Array prototype slice
+  const foo = ["a", "b", "c", "d", "e", "f", "g"];
+
+  console.log(foo.slice());
+  console.log(foo.slice(3));
+  console.log(foo.slice(3, 5));
+  console.log(foo.slice(-5));
+  console.log(foo.slice(-5, -2));
+}
+
+{
+  // Array prototype splice
+  const foo = [1, 2, 3, 4, 5, 6, 7, 8];
+  foo.splice(1, 3, 5);
+  console.log(foo);
+}
+
+{
+  // Array prototype join
+  const foo = ["a", "b", "c", "d", "e", "f", "g"];
+  console.log(foo.join());
+  console.log(foo.join("-"));
+  console.log(foo.join("#"));
+  console.log(foo.join(""));
+  console.log(foo.join(" "));
+}
+
+{
+  // Array prototype forEach
+  const foo = ["a", "b", "c", "d", "e", "f", "g"];
+  foo.forEach(function () {
+    console.log("JavaScript");
+  });
+}
