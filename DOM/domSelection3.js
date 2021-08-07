3af7d5547c8f48a27d6484bfbf3bc6bc96a6e9c1
Jauharuddin <jauharnotes@gmail.com>
2021-08-07 09:26:43 +0700

    DOM Selection Part 3

---
+++ DOM/domSelection3.js
@@ -0,0 +1,7 @@
+// const sectionB = document.getElementById("b");
+// const p4 = sectionB.querySelector("p");
+// p4.style.backgroundColor = "orange";
+
+const sectionB = document.querySelector("#b");
+const p4 = sectionB.getElementsByTagName("p")[0];
+p4.style.backgroundColor = "orange";
