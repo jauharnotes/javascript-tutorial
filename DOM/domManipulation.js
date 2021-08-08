f57b7d40c1e72174ef09c0dd84886f2f991ee352
Jauharuddin <jauharnotes@gmail.com>
2021-08-08 10:11:43 +0700

    DOM Manipulation Part 3

--- DOM/domManipulation.js
+++ DOM/domManipulation.js
@@ -11,9 +11,37 @@
 const newP = document.createElement("p");
 const textPNew = document.createTextNode("Paragraf baru");
 
-// simpan tulisan ke dalam paragraf
+// // simpan tulisan ke dalam paragraf
 newP.appendChild(textPNew);
 
-// simpan newP di akhir section A
+// // simpan newP di akhir section A
 const sectionA = document.getElementById("a");
 sectionA.appendChild(newP);
+
+// menambahkan element baru menggunakan inserBefore
+const newLi = document.createElement("li");
+const textNew = document.createTextNode("Item baru");
+newLi.appendChild(textNew);
+
+const ul = document.querySelector("section#b ul");
+const li2 = ul.querySelector("li:nth-child(2)");
+
+ul.insertBefore(newLi, li2);
+
+// menghapus element
+const link = document.getElementsByTagName("a")[0];
+sectionA.removeChild(link);
+
+// menganti element baru menggunakan replaceChild
+const sectionB = document.getElementById("b");
+const p4 = sectionB.querySelector("p");
+
+const h2New = document.createElement("h2");
+const newTextH2 = document.createTextNode("Judul Baru");
+h2New.appendChild(newTextH2);
+
+sectionB.replaceChild(h2New, p4);
+
+h2New.style.backgroundColor = "green";
+newP.style.backgroundColor = "green";
+newLi.style.backgroundColor = "green";
