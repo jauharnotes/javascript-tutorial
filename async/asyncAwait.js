b81a022efae7e442770c7e1d96f5cadb51b428ed
Jauharuddin <jauharnotes@gmail.com>
2021-08-19 23:03:17 +0700

    ajax

--- async/asyncAwait.js
+++ async/asyncAwait.js
@@ -1,40 +1,49 @@
-function getProductsUrl(keyword) {
-  return `https://www.blibli.com/backend/search/products?searchTerm=${keyword}`;
+function getProductsUrl() {
+  let key = document.getElementById("keyword").value;
+  return `http://www.omdbapi.com/?apikey=15a20b3e&s=${key}`;
 }
 
-function getProducts(keyword) {
-  return new Promise(function (resolve, reject) {
-    const ajax = new XMLHttpRequest();
-    ajax.onload = function () {
-      if (ajax.status === 200) {
-        const data = JSON.parse(ajax.responseText);
-        resolve(data);
-      } else {
-        reject(Error(ajax.statusText));
-      }
-    };
-    ajax.open("GET", getProductsUrl(keyword));
-    ajax.send();
-  });
-}
+// function getProducts(keyword) {
+//   return new Promise(function (resolve, reject) {
+//     const ajax = new XMLHttpRequest();
+//     ajax.onload = function () {
+//       if (ajax.response === 4 && ajax.status === 200) {
+//         const data = JSON.parse(ajax.responseText);
+//         resolve(data);
+//       } else {
+//         reject(Error(ajax.statusText));
+//       }
+//     };
+//     ajax.open("GET", getProductsUrl(keyword));
+//     ajax.send();
+//   });
+// }
+const ajax = new XMLHttpRequest();
+ajax.onreadystatechange = function () {
+  ajax.onload = function () {
+    if (ajax.response === 4 && ajax.status === 200) {
+      const data = JSON.parse(ajax.responseText);
+      document
+        .getElementById("btn-search")
+        .addEventListener("click", function () {
+          console.log(data);
+        });
+    } else {
+      alert("Error");
+    }
+  };
+};
+const url = getProductsUrl();
+ajax.open("GET", url);
+ajax.send();
 
 function clearProducts() {
   const productUl = document.getElementById("products");
   productUl.textContent = "";
 }
 
-function displayProducts(data) {
-  data.data.products.forEach((product) => displayProduct(product));
-}
-
-function displayProduct(product) {
-  const productLi = document.createElement("li");
-  productLi.textContent = product.name;
-
-  const productUl = document.getElementById("products");
-  productUl.appendChild(productLi);
-}
-
-function buttonClick() {
-  // Code Async Await Here!
-}
+// function buttonClick() {
+//   // Code Async Await Here!
+//   let key = document.getElementById("keyword").value;
+//   getProductsUrl(key);
+// }
