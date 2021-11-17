f629c32077ee670c53a5a31e5f0f6b4609e9fc10
Jauharuddin <jauharnotes@gmail.com>
2021-11-17 11:45:39 +0700

    promise

--- async/promise.js
+++ async/promise.js
@@ -1,14 +1,42 @@
-const promise = new Promise((resolve, reject) => {
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", "api/product.json");
-  xhr.onload = () => {
-    if (xhr.status === 200) {
-      resolve(xhr.responseText);
-    } else {
-      reject(`Error Get Product with response: ${xhr.responseText}`);
-    }
-  };
-  xhr.send();
-});
+// const promise = new Promise((resolve, reject) => {
+//   const xhr = new XMLHttpRequest();
+//   xhr.open("GET", "api/product.json");
+//   xhr.onload = () => {
+//     if (xhr.status === 200) {
+//       resolve(xhr.responseText);
+//     } else {
+//       reject(`Error Get Product with response: ${xhr.responseText}`);
+//     }
+//   };
+//   xhr.send();
+// });
 
-console.info(promise);
+// console.info(promise);
+
+function getProducts() {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "api/product.json");
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        resolve(xhr.responseText);
+      } else {
+        reject(`Error Get Product with response: ${xhr.status}`);
+      }
+    };
+    xhr.send();
+  });
+}
+
+getProducts()
+  .then((response) => JSON.parse(response))
+  .then((json) => {
+    document.getElementById("id").textContent = json.id;
+    document.getElementById("name").textContent = json.name;
+    document.getElementById("price").textContent = json.price;
+  })
+  .catch((error) => {
+    document.getElementById("id").textContent = error;
+    document.getElementById("name").textContent = error;
+    document.getElementById("price").textContent = error;
+  });
